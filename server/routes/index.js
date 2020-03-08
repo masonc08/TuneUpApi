73e87bb11641a95d9d636090c9a0c53ca59ef989
Mason <32713681+masonc08@users.noreply.github.com>
2020-03-08 14:39:04 -0400

    add error handling to authorization endpoint (#7)
    * installed dependencies and added authentication endpoint
    
    * remove node modules
    
    * added error handling

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,16 @@ import { authorize } from '@/providers/spotify/authorize';
 
 
 const app = express();
-app.get('/api', (req, res) => {
+app.get('/v1', (req, res) => {
     res.send('Tune Up!');
 });
-app.get('/api/spotify/get_categories/', (req, res) => {
-    authorize(response => res.send(response));
+app.get('/v1/spotify/authorize/', (req, res) => {
+    try {
+        authorize(res);
+    } catch (e) {
+        res.status(500).send(e);
+    }
 });
-
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
