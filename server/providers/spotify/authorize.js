c95618e8a14db699d04213eb100ad6f9005ed0e3
Mason Cao <mason.cao@freckleiot.com>
2020-03-08 13:44:53 -0400

    add error handling for missing spotify client keypair

--- server/providers/spotify/authorize.js
+++ server/providers/spotify/authorize.js
@@ -3,22 +3,28 @@ import { urls } from './urls';
 import btoa from 'btoa';
 import request from 'request';
 
-export const authorize = (send) => {
-    const b64auth = btoa(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`);
-    const options = {
-      'method': 'POST',
-      'url': urls.authorize,
-      'headers': {
-        'Authorization': `Basic ${b64auth}`,
-      },
-      form: {
-        'grant_type': 'client_credentials'
-      }
-    };
-    request(options, function (error, response) { 
-        if (error) {
-            throw new Error(error);
-        }
-        send(JSON.parse(response.body));
+export const authorize = (res) => {
+  if (!SPOTIFY_CLIENT_ID || !SPOTIFY_CLIENT_SECRET) {
+    res.status(401).send({
+      error: 'Spotify client keypair not detected. Are environment variables configured correctly?'
     });
+    return;
+  }
+  const b64auth = btoa(`${SPOTIFY_CLIENT_ID}:${SPOTIFY_CLIENT_SECRET}`);
+  const options = {
+    'method': 'POST',
+    'url': urls.authorize,
+    'headers': {
+      'Authorization': `Basic ${b64auth}`,
+    },
+    form: {
+      'grant_type': 'client_credentials'
+    }
+  };
+  request(options, function (error, response) { 
+      if (error) {
+          throw new Error(error);
+      }
+      res.status(response.statusCode).send(JSON.parse(response.body));
+  });
 };
