bae52263266ca2a4de287ef1bfdbb1242e969c3d
Mason Cao <mason.cao@freckleiot.com>
2020-03-08 16:20:40 -0400

    add_get_categories_endpoint

--- server/providers/spotify/urls.js
+++ server/providers/spotify/urls.js
@@ -1,3 +1,4 @@
 export const urls = {
     authorize: 'https://accounts.spotify.com/api/token',
+    get_categories: 'https://api.spotify.com/v1/browse/categories',
 };
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import { PORT } from '~/config';
-import { authorize } from '@/providers/spotify/authorize';
+import {
+    authorize,
+    get_categories,
+} from '@/providers/spotify';
 
 
 const app = express();
 app.get('/v1', (req, res) => {
     res.send('Tune Up!');
 });
+
 app.get('/v1/spotify/authorize/', (req, res) => {
     try {
         authorize(res);
@@ -14,6 +18,15 @@ app.get('/v1/spotify/authorize/', (req, res) => {
         res.status(500).send(e);
     }
 });
+
+app.get('/v1/spotify/get_categories/', (req, res) => {
+    if (!req.query.key) {
+        res.status(400).send('There was no auth key provided');
+        return;
+    }
+    get_categories(res, req.query.key);
+});
+
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}...`);
 });
