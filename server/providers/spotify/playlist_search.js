682c4b6ea7be6afe347e5bf91a42c49aa1fdc0f4
Mason <32713681+masonc08@users.noreply.github.com>
2020-03-22 13:09:11 -0400

    Bugfix: Correct Image URL Grab from API Response (#11)
    * add_get_categories_endpoint
    
    * added playlist search endpoint
    
    * updated endpoint onto readme
    
    * added href to response
    
    * added playlist_search to readme
    
    * added additional error catches
    
    * fix image grabber from api response

--- server/providers/spotify/playlist_search.js
+++ server/providers/spotify/playlist_search.js
@@ -1,49 +1,41 @@
 import request from 'request';
 import { urls } from './';
 
-const get_image = images => {
-    for (const image of images) {
-        if (image['height'] >= 200 && image['width'] >= 200 && image['url']) {
-            return image['url'];
-        }
-    }
-    return '';
-}
 
 const process_results = body => {
-    const response = {};
-    response['href'] = body['playlists']['href'];
-    response['playlists'] = [];
-    for (const item of body['playlists']['items']) {
-        response['playlists'].push({
-            'name': item['name'] || '',
-            'description': item['description'] || '',
-            'href': item['href'] || '',
-            'image': get_image(item['images']),
-        });
-    }
-    return response;
+  const response = {};
+  response['href'] = body['playlists']['href'];
+  response['playlists'] = [];
+  for (const item of body['playlists']['items']) {
+    response['playlists'].push({
+      'name': item['name'] || '',
+      'description': item['description'] || '',
+      'href': item['href'] || '',
+      'image': item['images'][0]['url'] || '',
+    });
+  }
+  return response;
 }
 
 export const playlist_search = (res, key, query) => {
-    const options = {
-      'method': 'GET',
-      'url': urls.playlist_search(query),
-      'headers': {
-        'Authorization': `Bearer ${key}`
-      }
-    };
-    request(options, (error, response) => { 
-        if (error) {
-            throw new Error(error);
-        }
-        const query_results = JSON.parse(response.body);
-        if (response.statusCode != 200) {
-            res.status(response.statusCode).send(query_results);
-            return;
-        }
-        res.status(response.statusCode).send(
-            process_results(query_results)
-        );
-    });    
+  const options = {
+    'method': 'GET',
+    'url': urls.playlist_search(query),
+    'headers': {
+      'Authorization': `Bearer ${key}`
+    }
+  };
+  request(options, (error, response) => { 
+    if (error) {
+      throw new Error(error);
+    }
+    const query_results = JSON.parse(response.body);
+    if (response.statusCode != 200) {
+      res.status(response.statusCode).send(query_results);
+      return;
+    }
+    res.status(response.statusCode).send(
+      process_results(query_results)
+    );
+  });    
 };
