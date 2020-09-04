c757f9c66e30f4ca890c0169ca834345e491eee3
Mason <mxmcao@gmail.com>
2020-09-04 11:29:07 -0400

    add timestampping to logger middleware

--- server/middleware/logger.js
+++ server/middleware/logger.js
@@ -1,4 +1,7 @@
+import moment from 'moment';
+
+
 export default (req, _, next) => {
-  console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`);
+  console.log(`[${moment().format()}] ${req.protocol}://${req.get('host')}${req.originalUrl}`);
   next();
 };
