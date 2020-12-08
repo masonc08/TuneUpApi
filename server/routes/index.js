670499244a77d6d81db6034a1e5bf641e1b979ca
Mason <mason.cao@uwaterloo.ca>
2020-12-07 23:32:59 -0500

    add cors fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -15,7 +15,7 @@ const app = express();
 // app.options('*', cors());
 app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
