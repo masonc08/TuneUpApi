a7e05953380d7ac6f16ace19002caf3f8991c4af
Mason <mason.cao@uwaterloo.ca>
2020-12-07 23:45:07 -0500

    remove legaccy file, add cors header

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import cors from 'cors';
 
 import loggerMiddleware from '@/middleware/logger';
 import spotifyRoute from '@/routes/spotify';
@@ -8,6 +9,7 @@ import { PORT } from '~/config';
 const app = express();
 
 app.use(loggerMiddleware);
+app.use(cors());
 app.use('/v1/spotify', spotifyRoute);
 app.get('/v1', (_, res) => {
   res.send('Tune Up!');
