b90da457434d20f2412b9972af8b7517ae76fa2d
Mason <mason.cao@uwaterloo.ca>
2020-12-07 23:28:02 -0500

    add cors fix

--- server/routes/index.js
+++ server/routes/index.js
@@ -12,7 +12,12 @@ import {
 
 const app = express();
 app.use(cors({credentials: true, origin: true}));
-
+app.options('*', cors());
+app.all('/*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
+});
 
 app.get('/v1/', (req, res) => {
   res.send('Tune Up!');
